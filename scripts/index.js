094aef2008ccce544b3252a488c73ccfcec7248c
Artem Belokonny <volorel@yandex.ru>
2020-08-31 18:56:05 +0300

    Второе ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,38 @@
-console.log('Привет, Артем!');
-
 const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.js-profile__open-popup');
+const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.popup__close');
-const nameInput = popup.querySelector('.popup__name');
-const jobInput = popup.querySelector('.popup__job');
-const popupName = popup.querySelector('.popup__name');
-const popupJob = popup.querySelector('.popup__job');
 const name = document.querySelector('.profile__name');
 const job = document.querySelector('.profile__job');
+const popupName = popup.querySelector('.popup__input_name');
+const popupJob = popup.querySelector('.popup__input_job');
 const formElement = popup.querySelector('.popup__container');
 
-const popupToggle = function() {
+function profileDate() {
     popupName.value = name.textContent;
     popupJob.value = job.textContent;
-    popup.classList.toggle('popup_opened')
 }
 
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
+function saveFormData() {
+    name.textContent = popupName.value;
+    job.textContent = popupJob.value;
+}
+
+function popupOpen(event) {
+    profileDate();
+    popup.classList.add('popup_opened');
+    
+}
+
+function popupClose(event) {
+    popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    name.textContent = popupName.value;
-    job.textContent = popupJob.value;
-    popupToggle()
+    saveFormData();
+    popupClose();
 }
 
+popupOpenButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler);
